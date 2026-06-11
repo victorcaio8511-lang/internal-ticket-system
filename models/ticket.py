4d784749f3f16ef7061bf1c1569eb4147fd7fb4c
victorcaio8511-lang <victorcaio8511@gmail.com>
2026-06-11 17:45:49 +0000

    Adiciona modelo Ticket

---
+++ models/ticket.py
@@ -0,0 +1,7 @@
+class Ticket:
+    def __init__(self, title, description, priority, status, responsible):
+        self.title = title
+        self.description = description
+        self.priority = priority
+        self.status = status
+        self.responsible = responsible
